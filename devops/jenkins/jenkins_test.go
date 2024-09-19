0f67ca43ca0bef9d520a030008e559fdefb6caf2
darrenyue <darrenxyue@gmail.com>
2024-09-19 09:44:50 +0800

    feat: pipeline路径修改

--- devops/jenkins/jenkins_test.go
+++ devops/jenkins/jenkins_test.go
@@ -8,20 +8,20 @@ import (
 )
 
 func TestCreateJenkins(t *testing.T) {
-	jenkins := CreateJenkins(nil, "http://114.55.35.38:13000/", 100, "admin", "Jimoretech0226")
+	jenkins := CreateJenkins(nil, "http://localhost:13000/", 100, "admin", "admin")
 	job, err := jenkins.GetJob("zeus-be")
-	apiURL, err := url.Parse("http://114.55.172.45:30180/")
+	apiURL, err := url.Parse("http://localhost:30180/")
 	param := &devops.HttpParameters{
 		Method: "GET",
 		Url:    apiURL,
 	}
-	pipeline, err := jenkins.GetPipeline("jimore-1lkh2b", "zeus-be", param)
+	pipeline, err := jenkins.GetPipeline("test", "test", param)
 	if err != nil {
 		return
 	}
 	if err != nil {
 		return
 	}
-	fmt.Print(pipeline)
-	fmt.Print(job)
+	fmt.Println(pipeline.ScmSource)
+	fmt.Println(job)
 }
